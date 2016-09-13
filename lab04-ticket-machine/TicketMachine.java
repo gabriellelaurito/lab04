03f078ed27ebf9aac175632f384410a3c5a914e1
GABRIELLE LAURITO <gabriellelaurito@adelphi.internal>
2016-09-13 09:52:08 -0400

    please

--- lab04-ticket-machine/TicketMachine.java
+++ lab04-ticket-machine/TicketMachine.java
@@ -80,4 +80,24 @@ public class TicketMachine
     {
         balance = 0;
     }
+    
+    public void setPrice(int cost)
+    {
+        price = cost;
+    }
+    
+    public void discount(int amount)
+    {
+        price = price - amount;
+    }
+  
+    public void prompt()
+    {
+        System.out.println(“Please insert the correct amount of money.”)
+    }
+
+    public void showPrice()
+    {
+        System.out.println("The price of a ticket is"+ price+" cents") 
+    }
 }
